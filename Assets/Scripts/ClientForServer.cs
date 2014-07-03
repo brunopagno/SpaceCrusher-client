f7596032af5c2dffab0009eef238a75726152547
Bruno Pagno <brunopagno@gmail.com>
2014-07-02 23:26:42 -0300

    Updated some things, don't really remember what. But works and it's amazing.

--- Assets/Scripts/ClientForServer.cs
+++ Assets/Scripts/ClientForServer.cs
@@ -7,7 +7,7 @@ using System.Text;
 
 public class ClientForServer : MonoBehaviour {
 
-    private string ipAddress = "127.0.0.1";
+    public string ipAddress = "127.0.0.1";
     public int port = 3339;
 
     IPEndPoint remoteEndPoint;
@@ -16,14 +16,18 @@ public class ClientForServer : MonoBehaviour {
     string strMessage = "";
 
     void OnGUI() {
-        Rect rectObj = new Rect(40, 380, 200, 400);
+        Rect rectObj = new Rect(40, 40, 200, 400);
         GUIStyle style = new GUIStyle();
         style.alignment = TextAnchor.UpperLeft;
         GUI.Box(rectObj, "# UDPSend-Data\n127.0.0.1 " + port + " #\nshell> nc -lu 127.0.0.1  " + port + " \n", style);
 
-        strMessage = GUI.TextField(new Rect(40, 420, 140, 20), strMessage);
-        if (GUI.Button(new Rect(190, 420, 40, 20), "send")) {
+        strMessage = GUI.TextField(new Rect(40, 80, 140, 20), strMessage);
+        if (GUI.Button(new Rect(190, 80, 40, 20), "send")) {
             sendString(strMessage + "\n");
+        }
+        ipAddress = GUI.TextField(new Rect(40, 120, 140, 20), ipAddress);
+        if (GUI.Button(new Rect(190, 120, 40, 20), "set ip")) {
+            remoteEndPoint = new IPEndPoint(IPAddress.Parse(ipAddress), port);
         }
     }
 
