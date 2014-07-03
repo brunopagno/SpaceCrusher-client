9ad02ebdaca8d98b83da4e8f0af1f07b1280d693
Bruno Pagno <brunopagno@gmail.com>
2014-07-03 17:03:10 -0300

    Amazing update that makes stuff work

--- Assets/Scripts/ClientForServer.cs
+++ Assets/Scripts/ClientForServer.cs
@@ -7,47 +7,37 @@ using System.Text;
 
 public class ClientForServer : MonoBehaviour {
 
-    public string ipAddress = "127.0.0.1";
-    public int port = 3339;
+    public string serverIpAddress = "127.0.0.1";
+    public int serverPort = 32154;
 
-    IPEndPoint remoteEndPoint;
-    UdpClient client;
+    public string errors = "";
 
-    string strMessage = "";
-    string textii = "";
-    string debugue = "";
-
-    void OnGUI() {
-        Rect rectObj = new Rect(40, 40, 200, 400);
-        GUIStyle style = new GUIStyle();
-        style.alignment = TextAnchor.UpperLeft;
-        GUI.Box(rectObj, "# UDPSend-Data\n127.0.0.1 " + port + " #\nshell> nc -lu 127.0.0.1  " + port + " \n", style);
-
-        strMessage = GUI.TextField(new Rect(40, 80, 240, 40), strMessage);
-        if (GUI.Button(new Rect(290, 80, 100, 40), "send")) {
-            sendString(strMessage + "\n");
-        }
-        ipAddress = GUI.TextField(new Rect(40, 120, 240, 40), ipAddress);
-        GUI.Label(new Rect(40, 185, 200, 40), "current ip-> " + textii);
-        if (GUI.Button(new Rect(290, 120, 100, 40), "set ip")) {
-            remoteEndPoint = new IPEndPoint(IPAddress.Parse(ipAddress), port);
-            client = new UdpClient();
-            textii = ipAddress;
-        }
-
-        GUI.Label(new Rect(40, 220, 300, 200), debugue);
+    void ConnectToServer() {
+        NetworkConnectionError netError = Network.Connect(serverIpAddress, serverPort);
+        errors += netError.ToString() + "\n";
     }
 
-    private void sendString(string message) {
-        try {
-            Debug.Log("OMG SENDING GODDAMNED DATA => " + message);
-            byte[] data = Encoding.UTF8.GetBytes(message);
+    void DisconnectFromServer() {
+        Network.Disconnect();
+    }
 
-            client.Send(data, data.Length, remoteEndPoint);
-            debugue = "SentData -> " + message + "\n" +
-                "Remote -> " + remoteEndPoint.Address + " port ->" + remoteEndPoint.Port;
-        } catch (Exception err) {
-            Debug.LogError(err.ToString());
+    void OnGUI() {
+        serverIpAddress = GUI.TextField(new Rect(40, 80, 240, 40), serverIpAddress);
+        GUI.Label(new Rect(40, 150, 240, 80), errors);
+        if (Network.peerType == NetworkPeerType.Disconnected) {
+            GUILayout.Label("Disconnected");
+            if (GUILayout.Button("Connect")) {
+                ConnectToServer();
+            }
+        } else {
+            if (Network.peerType == NetworkPeerType.Connecting) {
+                GUILayout.Label("Connecting");
+            } else {
+                GUILayout.Label("Connected");
+            }
+            if (GUILayout.Button("Disconnect")) {
+                DisconnectFromServer();
+            }
         }
     }
 }
