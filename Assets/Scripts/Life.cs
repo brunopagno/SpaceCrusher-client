4a1cc0bf5c368f2bc7b035d1ad079a7c6a6d56a0
Bruno Pagno <brunopagno@gmail.com>
2015-02-09 14:23:46 -0200

    Diogo's code for mobile vibration alerts

---
+++ Assets/Scripts/Life.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Life : MonoBehaviour {
+
+    public GUIText label;
+    private int amount = 0;
+    bool startVibration = false;
+    public int Amount {
+        get { return this.amount; }
+        set {
+            if (value < amount) {
+                startVibration = true;
+            }
+            this.amount = value;
+            this.label.text = amount.ToString() + "x";
+        }
+    }
+
+    void Update() {
+        if (startVibration) {
+            Handheld.Vibrate();
+            startVibration = false;
+        }
+    }
+
+}
